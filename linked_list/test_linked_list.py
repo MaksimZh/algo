0ee9d87c3550dbd3e1be24cb5cd04b4602552da1
Maksim Zholudev <maximzms@gmail.com>
2022-06-18 19:41:05 +0300

    Add linked list, tests and delete method

---
+++ linked_list/test_linked_list.py
@@ -0,0 +1,62 @@
+import unittest
+
+from linked_list import Node, LinkedList
+
+
+class Test(unittest.TestCase):
+
+    def checkList(self, l, test):
+        if not test:
+            self.assertIsNone(l.head)
+            self.assertIsNone(l.tail)
+        else:
+            self.assertIs(l.tail, test[-1])
+            i = 0
+            node = l.head
+            while node != None:
+                self.assertIs(node, test[i])
+                node = node.next
+                i += 1
+
+    def test_add(self):
+        l = LinkedList()
+        n1 = Node(12)
+        n2 = Node(55)
+        self.checkList(l, [])
+        l.add_in_tail(n1)
+        self.checkList(l, [n1])
+        l.add_in_tail(n2)
+        self.checkList(l, [n1, n2])
+
+    def test_find(self):
+        l = LinkedList()
+        self.assertIs(l.find(2), None)
+        nodes = [Node(0), Node(1), Node(2), Node(3), Node(2)]
+        for node in nodes:
+            l.add_in_tail(node)
+        self.assertIs(l.find(2), nodes[2])
+        self.assertIs(l.find(4), None)
+
+    def test_delete(self):
+        l = LinkedList()
+        nodes = [Node(0), Node(1), Node(2), Node(3), Node(2)]
+        for node in nodes:
+            l.add_in_tail(node)
+        l.delete(4)
+        self.checkList(l, nodes)
+        l.delete(2)
+        self.checkList(l, nodes[:2] + nodes[3:])
+        l.delete(2)
+        self.checkList(l, nodes[:2] + nodes[3:4])
+        l.delete(0)
+        self.checkList(l, [nodes[1], nodes[3]])
+        l.delete(1)
+        self.checkList(l, [nodes[3]])
+        l.delete(3)
+        self.checkList(l, [])
+        l.delete(3)
+        self.checkList(l, [])
+
+
+if __name__ == '__main__':
+    unittest.main()
