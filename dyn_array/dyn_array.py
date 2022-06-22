379593c10353afc9ec4c06cae7848bc806bb8060
Maksim Zholudev <maximzms@gmail.com>
2022-06-22 10:46:24 +0300

    Add DA delete

--- dyn_array/dyn_array.py
+++ dyn_array/dyn_array.py
@@ -1,10 +1,15 @@
 import ctypes
 
 class DynArray:
+
+    min_fill = 0.5
+    min_cap = 16
+    extend_factor = 2
+    shrink_factor = 1/1.5
     
     def __init__(self):
         self.count = 0
-        self.capacity = 16
+        self.capacity = self.min_cap
         self.array = self.make_array(self.capacity)
 
     def __len__(self):
@@ -25,9 +30,22 @@ class DynArray:
         self.array = new_array
         self.capacity = new_capacity
 
+    def _extend(self):
+        new_size = int(self.extend_factor * self.capacity)
+        self.resize(new_size)
+
+    def _shrink_if_needed(self):
+        if (self.capacity == self.min_cap) or \
+            (self.count >= self.min_fill * self.capacity):
+            return
+        new_size = int(self.shrink_factor * self.capacity)
+        if new_size < self.min_cap:
+            new_size = self.min_cap
+        self.resize(new_size)
+
     def append(self, itm):
         if self.count == self.capacity:
-            self.resize(2*self.capacity)
+            self._extend()
         self.array[self.count] = itm
         self.count += 1
 
@@ -35,11 +53,16 @@ class DynArray:
         if i < 0 or i > self.count:
             raise IndexError('Index is out of bounds')
         if self.count == self.capacity:
-            self.resize(2*self.capacity)
-        for i in reversed(range(i, self.count)):
-            self.array[i + 1] = self.array[i]
+            self._extend()
+        for j in reversed(range(i, self.count)):
+            self.array[j + 1] = self.array[j]
         self.array[i] = itm
         self.count += 1
 
     def delete(self, i):
-        pass # удаляем объект в позиции i
+        if i < 0 or i >= self.count:
+            raise IndexError('Index is out of bounds')
+        for j in range(i, self.count - 1):
+            self.array[j] = self.array[j + 1]
+        self.count -= 1
+        self._shrink_if_needed()
