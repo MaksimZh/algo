7c16dfcc759d98430f31d6370a270159b5ce204b
Maksim Zholudev <maximzms@gmail.com>
2022-06-29 14:25:41 +0300

    Add alternative stack implementation

--- stack/test_stack.py
+++ stack/test_stack.py
@@ -1,12 +1,10 @@
 import unittest
 
-from stack import Stack
 
-
-class Test(unittest.TestCase):
+class AbstractTest(unittest.TestCase):
 
     def test_0(self):
-        s = Stack()
+        s = self.Stack()
         self.assertEqual(s.size(), 0)
         self.assertIsNone(s.peek())
         self.assertEqual(s.size(), 0)
@@ -14,7 +12,7 @@ class Test(unittest.TestCase):
         self.assertEqual(s.size(), 0)
     
     def test_1(self):
-        s = Stack()
+        s = self.Stack()
         s.push(1)
         self.assertEqual(s.size(), 1)
         self.assertEqual(s.peek(), 1)
@@ -25,7 +23,7 @@ class Test(unittest.TestCase):
         self.assertIsNone(s.pop())
 
     def test_10(self):
-        s = Stack()
+        s = self.Stack()
         for i in range(1, 11):
             s.push(i)
         for i in range(10, 0, -1):
@@ -34,6 +32,15 @@ class Test(unittest.TestCase):
             self.assertEqual(s.size(), i)
             self.assertEqual(s.pop(), i)
 
+import stack
+class Test(AbstractTest):
+    Stack = stack.Stack
+
+import stack_a
+class TestA(AbstractTest):
+    Stack = stack_a.Stack
+
+del AbstractTest
 
 if __name__ == '__main__':
     unittest.main()
