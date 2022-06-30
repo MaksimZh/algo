60e90c8d9a4db971c1562dd157a45b5cf5bc3e3c
Maksim Zholudev <maximzms@gmail.com>
2022-06-30 10:15:06 +0300

    Implement queue

--- queue/queue.py
+++ queue/queue.py
@@ -1,13 +1,33 @@
 class Queue:
     def __init__(self):
-        pass # инициализация хранилища данных
+        self.head = Node(None)
+        self.tail = Node(None)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self._size = 0
 
     def enqueue(self, item):
-        pass # вставка в хвост
+        node = Node(item)
+        self.tail.prev.next = node
+        node.prev = self.tail.prev
+        node.next = self.tail
+        self.tail.prev = node
+        self._size += 1
 
     def dequeue(self):
-        # выдача из головы
-        return None # если очередь пустая
+        node = self.head.next
+        if self._size > 0:
+            self.head.next = node.next
+            node.next.prev = self.head
+            self._size -= 1
+        return node.value
 
     def size(self):
-        return 0 # размер очереди
+        return self._size
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.prev = None
+        self.next = None
--- queue/test_queue.py
+++ queue/test_queue.py
@@ -4,8 +4,29 @@ from queue import Queue
 
 class Test(unittest.TestCase):
 
-    def test(self):
-        pass
+    def test_0(self):
+        q = Queue()
+        self.assertEqual(q.size(), 0)
+        self.assertIsNone(q.dequeue())
+        self.assertEqual(q.size(), 0)
+
+    def test_1(self):
+        q = Queue()
+        q.enqueue(1)
+        self.assertEqual(q.size(), 1)
+        self.assertEqual(q.dequeue(), 1)
+        self.assertEqual(q.size(), 0)
+        self.assertIsNone(q.dequeue())
+        self.assertEqual(q.size(), 0)
+
+    def test_10(self):
+        q = Queue()
+        for i in range(1, 11):
+            q.enqueue(i)
+        for i in range(1, 11):
+            self.assertEqual(q.size(), 11 - i)
+            self.assertEqual(q.dequeue(), i)
+
 
 if __name__ == '__main__':
     unittest.main()
