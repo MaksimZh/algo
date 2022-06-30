2c5468e228b67f9b78846fdd97e2c8f9afc1ab1f
Maksim Zholudev <maximzms@gmail.com>
2022-06-30 10:46:52 +0300

    Improve test

--- queue/test_queue.py
+++ queue/test_queue.py
@@ -26,6 +26,33 @@ class AbstractTest(unittest.TestCase):
             self.assertEqual(q.size(), 11 - i)
             self.assertEqual(q.dequeue(), i)
 
+    def test_10_x(self):
+        q = self.Queue()
+        q.enqueue(1)
+        q.enqueue(2)
+        q.enqueue(3)
+        q.enqueue(4)
+        q.enqueue(5)
+        self.assertEqual(q.size(), 5)
+        self.assertEqual(q.dequeue(), 1)
+        self.assertEqual(q.dequeue(), 2)
+        self.assertEqual(q.dequeue(), 3)
+        self.assertEqual(q.size(), 2)
+        q.enqueue(6)
+        q.enqueue(7)
+        q.enqueue(8)
+        q.enqueue(9)
+        q.enqueue(10)
+        self.assertEqual(q.size(), 7)
+        self.assertEqual(q.dequeue(), 4)
+        self.assertEqual(q.dequeue(), 5)
+        self.assertEqual(q.dequeue(), 6)
+        self.assertEqual(q.dequeue(), 7)
+        self.assertEqual(q.dequeue(), 8)
+        self.assertEqual(q.dequeue(), 9)
+        self.assertEqual(q.dequeue(), 10)
+        self.assertEqual(q.size(), 0)
+
 import queue
 class Test(AbstractTest):
     Queue = queue.Queue
