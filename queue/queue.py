9c888de574c275b67cb54d81bf65297112d08541
Maksim Zholudev <maximzms@gmail.com>
2022-07-01 10:20:51 +0300

    Make queue use single linked list

--- queue/queue.py
+++ queue/queue.py
@@ -1,26 +1,23 @@
 class Queue:
     def __init__(self):
         self.head = Node(None)
-        self.tail = Node(None)
-        self.head.next = self.tail
-        self.tail.prev = self.head
+        self.tail = self.head
         self._size = 0
 
     def enqueue(self, item):
         node = Node(item)
-        self.tail.prev.next = node
-        node.prev = self.tail.prev
-        node.next = self.tail
-        self.tail.prev = node
+        self.tail.next = node
+        self.tail = node
         self._size += 1
 
     def dequeue(self):
-        node = self.head.next
         if self._size > 0:
+            node = self.head.next
             self.head.next = node.next
-            node.next.prev = self.head
             self._size -= 1
-        return node.value
+            return node.value
+        else:
+            return None
 
     def size(self):
         return self._size
@@ -29,5 +26,4 @@ class Queue:
 class Node:
     def __init__(self, value):
         self.value = value
-        self.prev = None
         self.next = None
